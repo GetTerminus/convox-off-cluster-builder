0f3c5b95388541dc364e9049ba45ad9ae46e772e
Brendan Erwin <brendanjerwin@gmail.com>
2018-01-03 13:12:20 -0500

    impl my interface on the V1 Manifest

--- manifest.go
+++ manifest.go
@@ -6,6 +6,24 @@ type Manifest interface {
 	Processes() []string
 }
 
+type V1Manifest manifest1.Manifest
+
 func LoadFile(path string) (Manifest, error) {
-	return manifest1.LoadFile(path)
+	man, err := manifest1.LoadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	v1man := V1Manifest(*man) //deref and cast the struct to my owned copy of the type
+	return &v1man, nil
+}
+
+func (m *V1Manifest) Processes() []string {
+	procs := []string{}
+	for k, _ := range m.Services {
+		procs = append(procs, k)
+	}
+
+	return procs
 }
